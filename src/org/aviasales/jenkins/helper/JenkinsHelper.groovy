6e0be3e8b3e408b3566746da7cf4dd20705396f4
Maxim Pogozhiy <foxdalas@gmail.com>
2019-03-08 17:11:40 +0700

    Small fixes

--- src/org/aviasales/jenkins/helper/JenkinsHelper.groovy
+++ src/org/aviasales/jenkins/helper/JenkinsHelper.groovy
@@ -36,3 +36,12 @@ def getFileChanges() {
       }
   }
 }
+
+def getLastSuccessfulCommit() {
+  def lastSuccessfulHash = null
+  def lastSuccessfulBuild = currentBuild.rawBuild.getPreviousSuccessfulBuild()
+  if ( lastSuccessfulBuild ) {
+    lastSuccessfulHash = commitHashForBuild(lastSuccessfulBuild)
+  }
+  return lastSuccessfulHash
+}
