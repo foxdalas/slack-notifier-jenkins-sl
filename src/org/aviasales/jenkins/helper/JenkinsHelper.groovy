795fd821ee112df211b241a31306203c0f30b33d
Maxim Pogozhiy <foxdalas@gmail.com>
2019-03-08 16:03:54 +0700

    Small fixes

--- src/org/aviasales/jenkins/helper/JenkinsHelper.groovy
+++ src/org/aviasales/jenkins/helper/JenkinsHelper.groovy
@@ -1,7 +1,5 @@
 package org.aviasales.jenkins.helper
 
-import hudson.plugins.accurev.*
-
 def BuildContainer(image, dockerFile, dir) {
   container('docker') {
     ansiColor('xterm') {
@@ -24,28 +22,5 @@ def PrepareGoPath(repoPrefix, projectName) {
 }
 
 def getFileChanges() {
-    def changes = "Changes: \n"
-    build = currentBuild
-    // Go through the previous builds and get changes until the
-    // last successful build is found.
-    while (build != null && build.result != 'SUCCESS') {
-        changes += "Build ${build.id}:\n"
 
-        for (changeLog in build.changeSets) {
-            for (AccurevTransaction entry in changeLog.items) {
-                changes += "\n    Issue: " + entry.getIssueNum()
-                changes += "\n    Change Type: " + entry.getAction()
-                changes += "\n    Change Message: " + entry.getMsg()
-                changes += "\n    Author: " + entry.getAuthor()
-                changes += "\n    Date: " + entry.getDate()
-                changes += "\n    Files: "
-                for (path in entry.getAffectedPaths()) {
-                    changes += "\n        " + path;
-                }
-                changes += "\n"
-            }
-        }
-        build = build.previousBuild
-    }
-    echo changes
 }
