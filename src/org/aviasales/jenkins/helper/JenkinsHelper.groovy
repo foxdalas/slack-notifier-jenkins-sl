976e3ca8e7e01c2541297d806f942c7a116e3371
Maxim Pogozhiy <foxdalas@gmail.com>
2019-03-08 15:30:50 +0700

    Small fixes

--- src/org/aviasales/jenkins/helper/JenkinsHelper.groovy
+++ src/org/aviasales/jenkins/helper/JenkinsHelper.groovy
@@ -1,16 +1,12 @@
 package org.aviasales.jenkins.helper
 
-def BuildContainer(name, image, dockerFile) {
-  return {
-    stage("Build #{name}") {
-      container('docker') {
-        ansiColor('xterm') {
-          def dockerImage = docker.build(image, "-f ${dockerFile} .")
-          dockerImage.push("${env.BRANCH_NAME}-${env.BUILD_NUMBER}")
-          if (env.BRANCH_NAME == 'master') {
-            dockerImage.push("stable")
-          }
-        }
+def BuildContainer(image, dockerFile, dir) {
+  container('docker') {
+    ansiColor('xterm') {
+      def dockerImage = docker.build(image, "-f ${dockerFile} ${dir}")
+      dockerImage.push("${env.BRANCH_NAME}-${env.BUILD_NUMBER}")
+      if (env.BRANCH_NAME == 'master') {
+        dockerImage.push("stable")
       }
     }
   }
