74f7aa74bcdc03b1ec26e63a7e274b2c2b0c6a15
Maxim Pogozhiy <foxdalas@gmail.com>
2019-03-28 15:46:49 +0700

    add integration tests notify

--- src/org/aviasales/jenkins/helper/JenkinsHelper.groovy
+++ src/org/aviasales/jenkins/helper/JenkinsHelper.groovy
@@ -60,3 +60,34 @@ def getLastSuccessfulCommit() {
   }
   return lastSuccessfulHash
 }
+
+def getRepoURL() {
+  sh "git config --get remote.origin.url > .git/remote-url"
+  return readFile(".git/remote-url").trim()
+}
+
+def getCommitSha() {
+  sh "git rev-parse HEAD > .git/current-commit"
+  return readFile(".git/current-commit").trim()
+}
+
+def notifyIntegrationTests(build) {
+  repoUrl = getRepoURL()
+  commitSha = getCommitSha()
+
+  step([
+    $class: 'GitHubCommitStatusSetter',
+    reposSource: [$class: "ManuallyEnteredRepositorySource", url: repoUrl],
+    commitShaSource: [$class: "ManuallyEnteredShaSource", sha: commitSha],
+    contextSource: [$class: "ManuallyEnteredCommitContextSource", context: "ci/jenkins/integrations-tests"],
+    errorHandlers: [[$class: 'ShallowAnyErrorHandler']],
+    statusResultSource: [
+      $class: 'ConditionalStatusResultSource',
+      results: [
+        [$class: 'BetterThanOrEqualBuildResult', result: 'SUCCESS', state: 'SUCCESS', message: build.description],
+        [$class: 'BetterThanOrEqualBuildResult', result: 'FAILURE', state: 'FAILURE', message: build.description],
+        [$class: 'AnyBuildResult', state: 'FAILURE', message: 'Loophole']
+      ]
+    ]
+  ])
+}
