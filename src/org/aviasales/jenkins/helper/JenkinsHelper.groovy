90f94cb08c2e0339346213508485dfa1a1c67e16
Maxim Pogozhiy <foxdalas@gmail.com>
2019-03-29 13:14:32 +0700

    More helpers

--- src/org/aviasales/jenkins/helper/JenkinsHelper.groovy
+++ src/org/aviasales/jenkins/helper/JenkinsHelper.groovy
@@ -3,11 +3,13 @@ package org.aviasales.jenkins.helper
 def BuildContainer(image, dockerFile, dir) {
   container('docker') {
     ansiColor('xterm') {
-      def dockerImage = docker.build(image, "-f ${dockerFile} ${dir}")
-      retry(5) {
-        dockerImage.push("${env.BRANCH_NAME}-${env.BUILD_NUMBER}")
-        if (env.BRANCH_NAME == 'master') {
-          dockerImage.push("stable")
+      docker.withRegistry('https://registry.hub.docker.com', 'dockerhub') {
+        def dockerImage = docker.build(image, "-f ${dockerFile} ${dir}")
+        retry(5) {
+          dockerImage.push("${env.BRANCH_NAME}-${env.BUILD_NUMBER}")
+          if (env.BRANCH_NAME == 'master') {
+            dockerImage.push("stable")
+          }
         }
       }
     }
