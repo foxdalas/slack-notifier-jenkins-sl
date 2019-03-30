d0e230811434556df94c6b71d90208bc9dfcd8a4
Maxim Pogozhiy <foxdalas@gmail.com>
2019-03-30 19:07:46 +0700

    Add getApplications

--- src/org/aviasales/jenkins/helper/JenkinsHelper.groovy
+++ src/org/aviasales/jenkins/helper/JenkinsHelper.groovy
@@ -75,6 +75,8 @@ def checkPrometheusAlerts(dir) {
 }
 
 def getApplications(dir) {
-  def subfolders = sh(returnStdout: true, script: 'ls -d assisted/*').trim().split(System.getProperty("line.separator"))
+  //def subfolders = sh(returnStdout: true, script: 'ls -d assisted/*').trim().split(System.getProperty("line.separator"))
+  def subfolders = sh(returnStdout: true, script: 'ls -d assisted/*').trim().split("/")
+
   return subfolders
 }
