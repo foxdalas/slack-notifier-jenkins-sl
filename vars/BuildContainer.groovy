cca030d0d110bd428cd61be810983af0bb17ad24
Maxim Pogozhiy <foxdalas@gmail.com>
2022-12-14 17:59:39 +0700

    add magic

--- vars/BuildContainer.groovy
+++ vars/BuildContainer.groovy
@@ -12,6 +12,14 @@ def call(credentialsId, image, dockerFile, dir) {
             dockerImage.push("latest")
           }
         }
+        def dockerImageECR = docker.build("667601112203.dkr.ecr.us-east-2.amazonaws.com/${image}", "-f ${dockerFile} ${dir}")
+        retry(5) {
+          dockerImageECR.push("${env.BRANCH_NAME}-${env.BUILD_NUMBER}")
+          if (env.BRANCH_NAME == 'master') {
+            dockerImageECR.push("stable")
+            dockerImageECR.push("latest")
+          }
+        }
       }
     }
   }
