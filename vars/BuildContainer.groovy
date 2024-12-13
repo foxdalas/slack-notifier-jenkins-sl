29642af9878b6e6de08aed04fca21e5a198b3b39
Alexey Kosenko <alko7enko@gmail.com>
2024-12-13 11:16:12 +0300

    chore(vars): delete push to dockerHub, update registry url (#3)

--- vars/BuildContainer.groovy
+++ vars/BuildContainer.groovy
@@ -7,15 +7,7 @@ def call(credentialsId, image, dockerFile, dir) {
         sh "mkdir -p /root/.docker"
         sh "cp $SecretFile ~/.docker/config.json"
 
-        def dockerImage = docker.build(image, "-f ${dockerFile} ${dir}")
-        retry(5) {
-          dockerImage.push("${env.BRANCH_NAME}-${env.BUILD_NUMBER}")
-          if (env.BRANCH_NAME == 'master') {
-            dockerImage.push("stable")
-            dockerImage.push("latest")
-          }
-        }
-        def dockerImageECR = docker.build("667601112203.dkr.ecr.us-east-2.amazonaws.com/${image}", "-f ${dockerFile} ${dir}")
+        def dockerImageECR = docker.build("${env.JENKINS_GLOBAL_REGISTRY_URL}/${image}", "-f ${dockerFile} ${dir}")
         retry(5) {
           dockerImageECR.push("${env.BRANCH_NAME}-${env.BUILD_NUMBER}")
           if (env.BRANCH_NAME == 'master') {
